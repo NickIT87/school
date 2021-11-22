cf64523c1d00362cd6b0eb7e39c4b2170e0a0817
Nickolazz1 <elanir358@gmail.com>
2021-11-22 18:59:38 +0200

    square equation

---
+++ task/square_equation.py
@@ -0,0 +1,39 @@
+from math import pow, sqrt
+
+
+# управляющая переменная цикла
+cycle = True
+
+print("Введите коэффициенты: \n")
+# цикл ввода коэффициентов 
+while cycle:
+    try:
+        a = float(input("a: "))
+        b = float(input("b: "))
+        c = float(input("c: "))
+        cycle = False
+    except:
+        print("Ошибка ввода, введите число!")
+
+# найдем дискриминант
+D = pow(b, 2) - 4 * a * c
+print(D)
+
+if D < 0:
+    print("Вещественных корней нет!")
+elif D == 0:
+    x = -b / (2*a)
+    print(f"x = {x}")
+else:
+    x1 = (-b + sqrt(D)) / (2 * a)
+    x2 = (-b - sqrt(D)) / (2 * a)
+    print(f"Корни уравнения: х1 = {x1}, x2 = {x2}")
+    # проверка
+    # print(a*pow(x1, 2) + b * x1 + c)  # = 0
+    # print(a*pow(x2, 2) + b * x2 + c)  # = 0
+
+
+# тестовые данные:
+# 4 4 4
+# 16 -8 1
+# 5 7 2
