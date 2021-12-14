88cf708af476f013193a5c88e19caa457cc602b0
Nickolazz1 <elanir358@gmail.com>
2021-12-14 10:52:46 +0200

    changes

--- individual/schooltask/student/views.py
+++ individual/schooltask/student/views.py
@@ -18,3 +18,15 @@ class HomePageView(ListView):
     def get_queryset(self):
         return Eleventh.objects.all()
 
+
+class Search(ListView):
+    template_name = 'student/searchResult.html'
+    context_object_name = 'found_obj'
+
+    def get_queryset(self):
+        pass
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "search"
+        return context
