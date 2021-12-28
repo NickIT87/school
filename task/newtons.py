3d7d2a4f64f2bd5fb1076e145bb1bedd50713904
Nickolazz1 <elanir358@gmail.com>
2021-12-28 10:58:18 +0200

    newtons 2 task

--- task/newtons.py
+++ task/newtons.py
@@ -1,14 +1,21 @@
 # какое ускорение приобретет тело массой m под действием силы F?
 # a = F / m
 
-def main() -> List[float]:
+def input_data(msg = 'm, F') -> float:
     while True:
         try:
-            m:float = float(input("Введите значение массы тела в килограммах: "))
-            print(m)
+            data:float = float(input(f"Введите значение - '{msg}': "))
+            return data
             break
         except:
-            print("try again")
+            print("Input error, try again!")
+
+
+def main():
+    m:float = input_data("m")
+    F:float = input_data("F")
+    a:float = F / m
+    print(f"Ответ: Ускорение = {a} м/с.")
 
 
 if __name__ == "__main__":
