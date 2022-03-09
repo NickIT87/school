a7e6f2220c1f637bef805355c361206cd28c3b1c
Nickolazz1 <elanir358@gmail.com>
2022-03-09 13:06:26 +0200

    l9_03_22

---
+++ remote/fahr.py
@@ -0,0 +1,24 @@
+def celsius_to_fahr(c: float):
+    return (9 / 5) * c + 32
+
+
+def fahr_to_celsius(f: float):
+    return (5 / 9) * (f - 32) 
+
+
+def start():
+    while True:
+        try:
+            usr_input = float(input("Введите число: \n"))
+            break
+        except:
+            print("Ошибка ввода!")
+
+    print(f"Т-ра по шкале Цельсия: {usr_input} в Фаренгейтах: ",
+        int(celsius_to_fahr(usr_input)))
+    print(f"Т-ра по шкале Френгейта: {usr_input} в Цельсиях: ",
+        int(fahr_to_celsius(usr_input)))
+    
+
+if __name__ == "__main__":
+    start()
