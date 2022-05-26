2bde3af01209f7d9074a8fe95a0cfe6681dbae56
Nickolazz1 <elanir358@gmail.com>
2022-05-26 14:25:26 +0300

    read token from file

--- ind/bot_telegram.py
+++ ind/bot_telegram.py
@@ -10,7 +10,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-TOKEN = ''
+TOKEN = '5339717364:AAEZ-fPgVQ8pKxEzVgDYxowGH5pvIyLKKKM'
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
--- ind/telegram_bots/mybot.py
+++ ind/telegram_bots/mybot.py
@@ -1,7 +1,13 @@
-import telebot
+import telebot                  # pip install pyTelegramBotAPI
 from telebot import types
 
-mytoken = ''
+
+with open(
+    '/home/nick/programming/school/ind/telegram_bots/mytoken.txt', 
+    'r'
+) as ftoken:
+    mytoken = ftoken.read()
+
 MypyBot = telebot.TeleBot(token=mytoken, parse_mode = None)
 
 
@@ -14,16 +20,20 @@ def start_message(message):
 def button_message(message):
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1=types.KeyboardButton("btn1")
-    #markup=types.InlineKeyboardMarkup()
-    #item1=types.InlineKeyboardButton("btn1", url='https://google.com')
-    markup.add(item1)
+    item2=types.KeyboardButton("btn2")
+    # markup=types.InlineKeyboardMarkup()
+    # item1=types.InlineKeyboardButton("btn1", url='https://google.com')
+    # item2=types.InlineKeyboardButton("btn2", url='https://youtube.com')
+    markup.add(item1, item2)
     MypyBot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
 
 
 @MypyBot.message_handler(content_types = ['text'])
 def replyer(message):
     if message.text == 'btn1':
-        MypyBot.reply_to(message, "что-то нажато")
+        MypyBot.reply_to(message, "выбрана опция 1")
+    elif message.text == 'btn2':\
+        MypyBot.reply_to(message, "выбрана опция 2")
     else:
         MypyBot.reply_to(message, message.text)
 
