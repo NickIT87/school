41b4d1e1c1cd9f743910d316527361c8fbc61f1d
NickIT87 <elanir358@gmail.com>
2022-09-07 20:34:06 +0300

    compress files

--- 9b/zip.py
+++ 9b/zip.py
@@ -6,9 +6,16 @@
 # jungle_zip.close()
 
 
-import zipfile
+# import zipfile
          
-fantasy_zip = zipfile.ZipFile('my.zip')
-fantasy_zip.extractall('')
+# fantasy_zip = zipfile.ZipFile('my.zip')
+# fantasy_zip.extractall('')
  
-fantasy_zip.close()
+# fantasy_zip.close()
+
+import zipfile
+       
+jungle_zip = zipfile.ZipFile('my_img.zip', 'w')
+jungle_zip.write('img.png', compress_type=zipfile.ZIP_DEFLATED)
+ 
+jungle_zip.close()
