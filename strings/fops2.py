c976ec61681535653c422658169c22d86adfbbb5
NickIT87 <elanir358@gmail.com>
2022-11-02 11:31:38 +0200

    fops school task

---
+++ strings/fops2.py
@@ -0,0 +1,23 @@
+# конструкція №1
+# open file for reading data
+with open("data.txt", "r") as f:
+    temp = f.read() # temp file data
+
+print(temp)
+print(type(temp), len(temp))
+# конструкція № 2
+# открыть файл
+f = open('data.txt', 'r')
+for line in f:
+    print(line)
+f.close()
+
+# конструкція з зберігання Строки у форматі JSON
+import json
+with open('test.json', encoding='utf-8') as json_file1:
+    q_base = json.load(json_file1)
+    y = json.dumps(json_file1)
+
+print(q_base)
+print(type(q_base))
+print(y, type(y))
