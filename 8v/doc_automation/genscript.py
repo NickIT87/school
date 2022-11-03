6e370aa433ce9298ce7e9a8492a595174a64b5c4
NickIT87 <elanir358@gmail.com>
2022-11-03 12:29:22 +0200

    file operations

--- 8v/doc_automation/genscript.py
+++ 8v/doc_automation/genscript.py
@@ -2,7 +2,7 @@ from docxtpl import DocxTemplate, RichText
 
 doc = DocxTemplate("my_word_template.docx")
 
-rt = RichText("hyperlink: ")
+rt = RichText("hyperlink: ", color='#ff00ff')
 rt.add(
     'google',
     url_id=doc.build_url_id('http://google.com'), 
@@ -10,7 +10,7 @@ rt.add(
 )
 
 context = { 
-    'company_name' : "8 в",
+    'company_name' : "10 б",
     'rt': rt
 }
 
--- strings/fops2.py
+++ strings/fops2.py
@@ -7,17 +7,19 @@ print(temp)
 print(type(temp), len(temp))
 # конструкція № 2
 # открыть файл
-f = open('data.txt', 'r')
-for line in f:
-    print(line)
+f = open('data.txt', 'r+')
+# for line in f:
+#     print(line)
+f.write("new data string\n second data")
+
 f.close()
 
 # конструкція з зберігання Строки у форматі JSON
-import json
-with open('test.json', encoding='utf-8') as json_file1:
-    q_base = json.load(json_file1)
-    y = json.dumps(json_file1)
+# import json
+# with open('test.json', encoding='utf-8') as json_file1:
+#     q_base = json.load(json_file1)
+#     y = json.dumps(json_file1)
 
-print(q_base)
-print(type(q_base))
-print(y, type(y))
+# print(q_base)
+# print(type(q_base))
+# print(y, type(y))
