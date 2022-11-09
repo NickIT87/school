49be0a664e8f6ce623679950edeae58c18e6d04c
NickIT87 <elanir358@gmail.com>
2022-11-09 13:00:22 +0200

    school tasks

--- 8v/doc_automation/genscript.py
+++ 8v/doc_automation/genscript.py
@@ -5,12 +5,12 @@ doc = DocxTemplate("my_word_template.docx")
 rt = RichText("hyperlink: ", color='#ff00ff')
 rt.add(
     'google',
-    url_id=doc.build_url_id('http://google.com'), 
-    color='#ff00ff'
+    url_id = doc.build_url_id('http://google.com'), 
+    color='#3a86ff'
 )
 
 context = { 
-    'company_name' : "10 б",
+    'company_name' : "8 в",
     'rt': rt
 }
 
