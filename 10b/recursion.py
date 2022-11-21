e6b4473ebfb69102365e21f3c7f37b59ef0d14d1
NickIT87 <elanir358@gmail.com>
2022-11-21 13:22:34 +0200

    recursion

--- 10b/recursion.py
+++ 10b/recursion.py
@@ -25,4 +25,25 @@ def nod(a, b):
         return nod(b, a % b)
 
 
-print(nod(45, 24))
+#print(nod(45, 24))
+result:list = []
+
+def recurs_find_even(num_list: list) -> list:
+    global result
+    if not num_list:
+        return 
+    else:
+        first_element: int = num_list[0]
+        if first_element % 2 == 0:  #додатне, якщо ділится на два без залишку
+            result.append(first_element)
+        recurs_find_even(num_list[1:])
+
+
+def recurs_find_key(key, obj):
+    if key in obj:
+        return obj[key] 
+    for k, v in obj.items ( ):
+        if type (v) == dict:
+            result = recurs_find_key(key, v)
+            if result is not None:
+                return result
