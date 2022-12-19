4c2ba534f92aab667bf74173d6b1da5589c6a347
NickIT87 <elanir358@gmail.com>
2022-12-19 17:31:14 +0200

    some tasks

---
+++ 10b/tkinterGUI/example1.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+
+def add(eve):
+    a = int(input("a: "))
+    b = int(input("b: "))
+    print(a + b)
+
+def multiply(fff):
+    c = int(input("c: "))
+    d = int(input("d: "))
+    print(c * d)
+
+root = Tk()
+
+but1 = Button(root)
+but1['text'] = 'adding'
+but1.bind("<Button>", add)
+
+but2 = Button(root)
+but2['text'] = 'multiply'
+but2.bind("<Button>", multiply)
+
+but1.pack()
+but2.pack()
+
+root.mainloop()
