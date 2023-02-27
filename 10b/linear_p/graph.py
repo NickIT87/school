9bf78e283bc3bc88e2c1b5d0dcbd8f448e8bcc33
NickIT87 <elanir358@gmail.com>
2023-02-27 13:44:23 +0200

    solve

---
+++ 10b/linear_p/graph.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Define the objective function and constraints
+def objective_function(x):
+    return 2 * x + 6
+
+def constraint1(x):
+    return (25 - 3 * x) / 4
+
+def constraint2(x):
+    return 10 - 2 * x
+
+# Create a grid of x and y values
+x = np.linspace(0, 10, 100)
+y1 = constraint1(x)
+y2 = constraint2(x)
+
+# Create the plot
+fig, ax = plt.subplots()
+ax.plot(x, objective_function(x), label='Objective Function')
+ax.plot(x, y1, label='Constraint 1')
+ax.plot(x, y2, label='Constraint 2')
+ax.axvline(0, color='black')
+ax.axhline(0, color='black')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.legend()
+
+# Find the intersection points of the constraints
+A = np.array([[3, 4], [2, 1]])
+b = np.array([25, 10])
+intersection = np.linalg.solve(A, b)
+
+# Plot the intersection points
+ax.plot(intersection[0], intersection[1], 'ro')
+ax.annotate('Optimal Point', xy=intersection, xytext=(intersection[0] - 1, intersection[1] + 1),
+            arrowprops=dict(facecolor='black', shrink=0.05))
+
+# Print the optimal solution
+print('Optimal value:', objective_function(intersection[0]))
+print('Optimal solution:', intersection)
+
+plt.show()
+
+#In this example, we define the objective function 2x + y and two constraints: 
+# 3x + 4y <= 25 and 2x + y <= 10. We create a grid of x and y values, 
+# plot the objective function and constraints, and find the intersection 
+# points of the constraints. We then plot the intersection points and annotate 
+# the optimal point, which is the intersection of the constraints that maximizes 
+# the objective function. Finally, we print the optimal value and solution.
--- 10b/linear_p/task1.py
+++ 10b/linear_p/task1.py
@@ -1,23 +1,19 @@
 import pulp
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Create a maximization problem
 problem = pulp.LpProblem("Maximization Problem", pulp.LpMaximize)
 
 # Define the decision variables
-# x = pulp.LpVariable('x', lowBound=0)
-# y = pulp.LpVariable('y', lowBound=0)
 a = pulp.LpVariable('a', lowBound=0)
 b = pulp.LpVariable('b', lowBound=0)
 c = pulp.LpVariable('c', lowBound=0)
 
 # Add the objective function
-#problem += 2 * x + y
 problem += 13 * a + 18 * b + 22 * c
 
 # Add the constraints
-# problem += 3 * x + 4 * y <= 25
-# problem += 2 * x + y <= 10
-
 problem += a + b + c == 300
 problem += a >= 50
 problem += b >= 40
@@ -33,3 +29,28 @@ print("Optimal Value:", pulp.value(problem.objective))
 # Print the optimal solution
 for var in problem.variables():
     print(var.name, "=", var.varValue)
+
+a_value = a.value()
+b_value = b.value()
+c_value = c.value()
+z_value = problem.objective.value()
+
+# plot the feasible region
+a_vals = np.linspace(0, 300, 100)
+b_vals1 = 300 - a_vals
+b_vals2 = 40 * np.ones_like(a_vals)
+c_vals = np.linspace(0, 40, 100)
+plt.plot(a_vals, b_vals1, 'r-', label='a + b + c = 300')
+plt.plot(a_vals, b_vals2, 'b-', label='c <= 40')
+plt.plot(50 * np.ones_like(c_vals), c_vals, 'g-', label='a >= 50')
+
+# plot the optimal solution
+plt.plot(a_value, b_value, 'go', markersize=10, label='Optimal solution')
+
+# add labels and legend
+plt.xlabel('a')
+plt.ylabel('b')
+plt.legend()
+
+# show the plot
+plt.show()
