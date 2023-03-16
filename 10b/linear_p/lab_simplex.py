b9bed61d09679039d6c14020d75f264503e31a3f
NickIT87 <elanir358@gmail.com>
2023-03-16 22:52:57 +0200

    preliminary design

--- 10b/linear_p/lab_simplex.py
+++ 10b/linear_p/lab_simplex.py
@@ -1,7 +1,7 @@
 from ortools.linear_solver import pywraplp
 
 # Створення симплекс-солвера
-solver = pywraplp.Solver.CreateSolver('simplex')
+solver = pywraplp.Solver.CreateSolver('GLOP')
 
 # Створення змінних
 x1 = solver.NumVar(0, solver.infinity(), 'x1')
@@ -26,12 +26,58 @@ if status == pywraplp.Solver.OPTIMAL:
     print('x2 =', x2.solution_value())
     print('Максимальне значення цільової функції:', solver.Objective().Value())
 
-    # Виведення симплекс-таблиць
-    print('\nСимплекс-таблиці:')
-    for i in range(solver.NumVariables()):
-        print('x{}:'.format(i + 1), end=' ')
-        for j in range(solver.NumConstraints() + 1):
-            print(solver.GetTableauCoefficient(j, i), end=' ')
-        print()
+    # # Виведення симплекс-таблиць
+    # print('\nСимплекс-таблиці:')
+    # for i in range(solver.NumVariables()):
+    #     print('x{}:'.format(i + 1), end=' ')
+    #     for j in range(solver.NumConstraints() + 1):
+    #         print(solver.GetTableauCoefficient(j, i), end=' ')
+    #     print()
 else:
     print('Проблема не вирішена')
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Функція обмеження
+def constraint1(x1):
+    return 500 - 4 * x1
+
+def constraint2(x1):
+    return 150 - x1
+
+def constraint3(x1):
+    return 300 - 4 * x1
+
+x1_vals = np.linspace(0, 200, 400)
+
+# Значення обмежень
+x2_1 = constraint1(x1_vals)
+x2_2 = np.full_like(x1_vals, 300)
+x2_3 = constraint2(x1_vals)
+x2_4 = constraint3(x1_vals)
+
+# Побудова графіку
+plt.plot(x1_vals, x2_1, label="4x1 + x2 <= 500")
+plt.plot(x1_vals, x2_2, label="x2 <= 300")
+plt.plot(x1_vals, x2_3, label="x1 + x2 <= 150")
+plt.plot(x1_vals, x2_4, label="4x1 + x2 <= 300")
+
+
+plt.xlim(0, 200)
+plt.ylim(0, 350)
+plt.xlabel("x1")
+plt.ylabel("x2")
+
+plt.fill_between(x1_vals, np.minimum(x2_1, np.minimum(x2_2, np.minimum(x2_3, x2_4))), color="gray", alpha=0.5)
+plt.plot(50, 100, 'bo', label='C (50, 100)')
+plt.legend()
+
+# Відображення оптимальної точки
+opt_x1 = x1.solution_value()
+opt_x2 = x2.solution_value()
+plt.scatter(opt_x1, opt_x2, color="red", marker="o", label="Оптимальна точка")
+plt.legend()
+plt.grid(True)
+plt.show()
