313611d0c3e3acee1b807b8ea369a9314b7f496c
NickIT87 <elanir358@gmail.com>
2023-03-29 18:23:47 +0300

    pbp

--- 10b/design/project/landing_school/src/components/NavbarMenu.js
+++ 10b/design/project/landing_school/src/components/NavbarMenu.js
@@ -5,12 +5,10 @@ const NavbarMenu = () => {
     <header>
       <nav>
         <div class="nav-wrapper">
-          <a href="#" class="brand-logo">Агентство недвижимости</a>
+          <a href="#" class="brand-logo">Презентація бізнес проектів</a>
           <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
           <ul class="right hide-on-med-and-down">
-            <li><a href="#about">О нас</a></li>
-            <li><a href="#services">Услуги</a></li>
-            <li><a href="#contact">Контакты</a></li>
+            <li><a href="https://martinschrader.pythonanywhere.com">RentalProject</a></li>
           </ul>
         </div>
       </nav>
