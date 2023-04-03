0c975814f62416fe861a22c0c49f6e6d3b442fdd
NickIT87 <elanir358@gmail.com>
2023-04-03 13:30:03 +0300

    some refactor

--- 10b/design/project/landing_school/src/components/NavbarMenu.js
+++ 10b/design/project/landing_school/src/components/NavbarMenu.js
@@ -1,19 +1,38 @@
 import React from 'react'
+import M from 'materialize-css/dist/js/materialize.min.js';
+import 'materialize-css/dist/css/materialize.min.css';
+import { useEffect } from 'react';
+
 
 const NavbarMenu = () => {
+
+  useEffect( () => {
+    const sidenav = document.querySelector('.sidenav');
+    M.Sidenav.init(sidenav);
+  }, []);
+
   return (
     <header>
       <nav>
         <div class="nav-wrapper">
-          <a href="#" class="brand-logo">Презентація бізнес проектів</a>
+          <a href="#" class="brand-logo">Аґенція нерухомості</a>
           <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
           <ul class="right hide-on-med-and-down">
             <li><a href="https://martinschrader.pythonanywhere.com">RentalProject</a></li>
           </ul>
         </div>
       </nav>
+
+      {/* Мобильное навигационное меню */}
+      <ul class="sidenav" id="mobile-demo">
+        <li><a href="#about">О нас</a></li>
+        <li><a href="#services">Услуги</a></li>
+        <li><a href="#contact">Контакты</a></li>
+      </ul>
     </header>
   )
 }
+
+
 
 export default NavbarMenu
