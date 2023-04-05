6e63934a2b850be3999c21fbd8006757bdbd17d3
NickIT87 <elanir358@gmail.com>
2023-04-05 10:50:47 +0300

    9b

--- 9b/project_utest/test_main.py
+++ 9b/project_utest/test_main.py
@@ -1,19 +1,16 @@
 from main import factorial
 import pytest
 
-
+#1
 def test_factorial_of_zero():
     assert factorial(0) == 1
 
-def test_factorial_of_positive_number():
-    assert factorial(5) == 120
-    assert factorial(10) == 3628800
-    assert factorial(7) == 5040
-
+#2
 def test_factorial_of_negative_number():
     with pytest.raises(ValueError):
         factorial(-1)
 
+#3
 def test_factorial_non_integer_input():
     with pytest.raises(TypeError):
         factorial("abc")
@@ -22,4 +19,8 @@ def test_factorial_non_integer_input():
     with pytest.raises(TypeError):
         factorial(True)
 
-
+#4
+def test_factorial_of_positive_number():
+    assert factorial(5) == 120
+    assert factorial(10) == 3628800
+    assert factorial(7) == 5040
