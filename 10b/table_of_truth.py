3501d20c9ef9325ca70574d90bc3f1e1c5b8a871
NickIT87 <elanir358@gmail.com>
2023-04-26 11:27:37 +0300

    scheme lesson

--- 10b/table_of_truth.py
+++ 10b/table_of_truth.py
@@ -1,30 +1,33 @@
 
 
 def print_data(x, y):
-    print("not x: ", not x )
-    print ("x or y: ", x or y)
-    print ("x and y: ", x and y)
-    print ("x ^ y: ", x ^ y)
-    print("################################################################")
+    print ("not x: ", not x )           # заперечення
+    print ("x or y: ", x or y)          # диз'юнкція
+    print ("x and y: ", x and y)        # кон'юнкція
+    print ("x ^ y: ", x ^ y)            # xor (ділення по модулю 2) "виключне або"
+    print ("x is y", x is y)            # еквіваленція
+    print ("x -> y", not x or y)        # імплікація
+
+    print("\n################################################################\n")
 
 
 
-# print_data(False, False)
-# print_data(True, False)
-# print_data(False, True)
-# print_data(True, True)
+print_data(False, False)
+print_data(True, False)
+print_data(False, True)
+print_data(True, True)
 
 
-a = {1, 3, 5, 7}
+# a = {1, 3, 5, 7}
 
-b = {2, 4, 6, 8}
+# b = {2, 4, 6, 8}
 
-c = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+# c = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
 
-d = set()
+# d = set()
 
 # print( not d )
 # print( d or b )
 # print( a and b )
-print(a ^ b)
-print(d ^ d)
+# print(a ^ b)
+# print(d ^ d)
--- bool_project/test.py
+++ bool_project/test.py
@@ -1,70 +1,71 @@
-def f(x, y, z):
-    return not (x or y) and x and y or (x and not y) and z
+# def f(x, y, z):
+#     return not (x or y) and x and y or (x and not y) and z
 
 
 
 
-print(f(False, False, False))
-print(f(True, True, True))
-print(f(True, False, False))
-print(f(False, True, False))
-print(f(False, False, True))
-print(f(False, True, True))
-print(f(True, False, True))
-print(f(True, True, False))
-
-
-
-
-
+# print(f(False, False, False))
+# print(f(True, True, True))
+# print(f(True, False, False))
+# print(f(False, True, False))
+# print(f(False, False, True))
+# print(f(False, True, True))
+# print(f(True, False, True))
+# print(f(True, True, False))
 
 
 
 
 
-# def a1(x1, x2) -> bool:
-#     return x1 or x2
-
-# def a2(a1) -> bool:
-#     return not a1
-
-# def a3(x1, x2) -> bool:
-#     return x1 and x2
-
-# def a4(a2, a3) -> bool:
-#     return a2 and a3
-
-# def a5(x2) -> bool:
-#     return not x2
-
-# def a6(x1, a5) -> bool:
-#     return x1 and a5
-
-# def a7(a6, x3) -> bool:
-#     return a6 and x3
-
-# def f(a4, a7) -> bool:
-#     return a4 or a7
 
 
-# x1, x2, x3 = 1, 0, 1
 
-# if __name__ == "__main__":
-#     #print(f(a4(a2(a1(x1, x2)), a3(x1, x2)), a7(a6(x1, a5(x2)), x3)))
-#     print(
-#         f(
-#             a4(
-#                 a2(
-#                     a1(x1, x2)
-#                 ), 
-#                 a3(x1, x2)
-#             ), 
-#             a7(
-#                 a6(
-#                     x1, 
-#                     a5(x2)
-#                 ), 
-#                 x3
-#             )
-#         )
-#     )
+
+def a1(x1, x2) -> bool:
+    return x1 or x2
+
+def a2(a1) -> bool:
+    return not a1
+
+def a3(x1, x2) -> bool:
+    return x1 and x2
+
+def a4(a2, a3) -> bool:
+    return a2 and a3
+
+def a5(x2) -> bool:
+    return not x2
+
+def a6(x1, a5) -> bool:
+    return x1 and a5
+
+def a7(a6, x3) -> bool:
+    return a6 and x3
+
+def f(a4, a7) -> bool:
+    return a4 or a7
+
+
+x1, x2, x3 = 1, 0, 1
+
+if __name__ == "__main__":
+    # print(
+    #     f(a4(a2(a1(x1, x2)), a3(x1, x2)), a7(a6(x1, a5(x2)), x3))
+    # )
+    print(
+        f(
+            a4(
+                a2(
+                    a1(x1, x2)
+                ), 
+                a3(x1, x2)
+            ), 
+            a7(
+                a6(
+                    x1, 
+                    a5(x2)
+                ), 
+                x3
+            )
+        )
+    )
