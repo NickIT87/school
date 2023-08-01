60948d6ae215b4a3d53d7bfaf4ea3d590947cb51
NickIT87 <elanir358@gmail.com>
2023-08-01 15:31:17 +0300

    mysql connection

---
+++ mysql/example.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+# Replace the placeholders with your MySQL database credentials
+db_config = {
+    'host': 'localhost',
+    'user': 'nick',
+    'password': '',
+    'database': 'test'
+}
+
+# Connect to MySQL server
+connection = mysql.connector.connect(**db_config)
+cursor = connection.cursor()
+
+#cursor.execute("CREATE DATABASE IF NOT EXISTS test")
+cursor.execute("CREATE TABLE IF NOT EXISTS Person (name VARCHAR(255), age smallint UNSIGNED, personID int PRIMARY KEY)")
+cursor.execute("INSERT INTO Person (name, age, personID) VALUES (%s, %s, %s)", ("Tim", 45, 1))
+connection.commit()
+cursor.execute("DESCRIBE Person")
+for x in cursor:
+    print(x)
+cursor.execute("SELECT * FROM Person")
+for x in cursor:
+    print(x)
+
+cursor.close()
+connection.close()
