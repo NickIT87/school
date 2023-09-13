372e8b0606f4afde70f9609ef9890bb85f3d332d
NickIT87 <elanir358@gmail.com>
2023-09-13 22:48:02 +0300

    esc btn press code

--- 9v/main.c
+++ 9v/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define esc 27
 
 int main()
 {
-    printf("Hello!\n");
+    char ch;
+    int exitflag = 0; // Initialize exitflag to 0
 
-    return 0;
+    do
+    {
+        ch = getchar(); // Use getchar() to read a character
+        switch(ch)
+        {
+            case esc:
+                printf("Escape key pressed. Exiting program.\n");
+                exitflag = 1;
+                break;
+        }
+    }
+    while(exitflag != 1);
+
+    return 0; // Return 0 to indicate successful execution
 }
