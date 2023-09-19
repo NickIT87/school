9e28a2a914d2dc400d1192f9b20bcd7a0f99d1fc
NickIT87 <elanir358@gmail.com>
2023-09-19 12:27:43 +0300

    statement 2

--- EOLYMP/conditional_statement.c
+++ EOLYMP/conditional_statement.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <math.h>
 
 short int solver (short int);
+short int solver2 (short int);
 
 int main()
 {
     short int y = 0;
     scanf("%hd", &y);
-    printf("y = %hd\n", solver(y));
+    //printf("y = %hd\n", solver(y));
+    printf("y = %hd\n", solver2(y));
     return 0;
 }
 
@@ -16,4 +19,12 @@ short int solver (short int x)
         return x * x - 3 * x + 4;
     else
         return x + 7;
+}
+
+short int solver2 (short int x)
+{
+    if (x >= 10) 
+        return pow(x, 3) + 5 * x;
+    else
+        return x * x - 2 * x + 4;
 }
