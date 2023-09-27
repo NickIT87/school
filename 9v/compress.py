0059984b5d00e68d14ea29610452b10a6e34aa65
NickIT87 <elanir358@gmail.com>
2023-09-27 12:28:30 +0300

    test zip

--- 9v/compress.py
+++ 9v/compress.py
@@ -1,14 +1,19 @@
+# підключення бібліотеки для архівування даних
 import zipfile
 
-
+# функція яка виконує архівування файлу
 def compress_file(input_filename, output_filename):
-    with zipfile.ZipFile(output_filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    # оператор контексту який створює програмний об'єкт та записує його в файл
+    #with zipfile.ZipFile(output_filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    #with zipfile.ZipFile(output_filename, 'w', zipfile.ZIP_BZIP2) as zipf:
+    with zipfile.ZipFile(output_filename, 'w', zipfile.ZIP_LZMA) as zipf:
+        # створити архів даних та записати його на диск
         zipf.write(input_filename, arcname=input_filename)
 
-
-def extract_zip(zip_filename, extract_path):
-    with zipfile.ZipFile(zip_filename, 'r') as zip_ref:
-        zip_ref.extractall(extract_path)
+# розпаковка інсуючого архіву
+# def extract_zip(zip_filename, extract_path):
+#     with zipfile.ZipFile(zip_filename, 'r') as zip_ref:
+#         zip_ref.extractall(extract_path)
 
 
 input_filename = 'a.out'
@@ -18,4 +23,4 @@ extract_path = 'extracted/'
 
 
 compress_file(input_filename, output_filename)
-extract_zip(zip_filename, extract_path)
+#extract_zip(zip_filename, extract_path)
