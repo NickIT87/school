64b8caa614971bda8a3d5a4c5ebc10b4dd017562
NickIT87 <elanir358@gmail.com>
2023-09-28 09:41:34 +0300

    simple DB gui

---
+++ 11b/DB/simpleGUI.py
@@ -0,0 +1,64 @@
+import tkinter as tk
+from tkinter import messagebox
+import sqlite3
+
+# Create a connection to the database (or create one if it doesn't exist)
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+
+# Create a table if it doesn't exist
+c.execute('''CREATE TABLE IF NOT EXISTS records
+             (id INTEGER PRIMARY KEY, name TEXT)''')
+conn.commit()
+
+# Function to view all records
+def view_records():
+    c.execute('SELECT * FROM records')
+    records = c.fetchall()
+    for record in records:
+        print(record)
+
+# Function to add a new record
+def add_record():
+    name = entry.get()
+    c.execute('INSERT INTO records (name) VALUES (?)', (name,))
+    conn.commit()
+    entry.delete(0, tk.END)
+    messagebox.showinfo('Success', 'Record added successfully')
+
+# Function to delete a record by ID
+def delete_record():
+    try:
+        record_id = int(entry.get())
+        c.execute('DELETE FROM records WHERE id = ?', (record_id,))
+        conn.commit()
+        entry.delete(0, tk.END)
+        messagebox.showinfo('Success', 'Record deleted successfully')
+    except ValueError:
+        messagebox.showerror('Error', 'Please enter a valid ID')
+
+# Create main window
+root = tk.Tk()
+root.title('SQLite3 GUI')
+
+# Labels and entry fields
+label = tk.Label(root, text='Enter Name:')
+label.pack(pady=10)
+entry = tk.Entry(root)
+entry.pack(pady=10)
+
+# Buttons for actions
+view_button = tk.Button(root, text='View Records', command=view_records)
+view_button.pack(padx=10, pady=10, side=tk.LEFT)
+
+add_button = tk.Button(root, text='Add Record', command=add_record)
+add_button.pack(padx=10, pady=10, side=tk.LEFT)
+
+delete_button = tk.Button(root, text='Delete Record', command=delete_record)
+delete_button.pack(padx=10, pady=10, side=tk.LEFT)
+
+# Run the GUI application
+root.mainloop()
+
+# Close the connection when done
+conn.close()
